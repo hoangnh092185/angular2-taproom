f9dda96ec556b1d63f724215a1c9f38a57ea1e20
Gray Hildreth and Nhat Hoang <Guest@epicodus-60.local>
2016-10-17 17:59:49 -0700

    add beer model.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,27 +1,39 @@
 import { Component } from '@angular/core';
+import { Task } from './beer.model';
 
 @
 Component({
     selector: 'my-app',
     template: `
     <div class="container">
-      <div class="row">
-      <div class="col-sm-8"><img src="./../resources/image/Gear_Up.png" alt="" /></div>
-      <div class="col-sm-4">
-        <h4>
-          " 14"
-          <sup>degree plato</sup>
-          " |65 "
-          <sup>ibu</sup>
-          " | "
-          <sup>ALC</sup>
-          " 6.2%"
-          <sup>By Volume</sup>
-        </h4>
-      </div>
-
+      <div class="container">
+        <div class="row beer" *ngFor="let beer of beers">
+          <div class="col-sm-8"><img src="./../resources/image/{{beer.image}}" alt="" /></div>
+          <div class="col-sm-4 jumbotron brew-note">
+            <ul class="list-group">
+            <h4>{{beer.title}}</h4>
+              <li class="list-group-item">{{beer.plato}}
+                <sup>degree plato</sup></li>
+              <li class="list-group-item">{{beer.ibu}}
+                <sup>ibu</sup></li>
+              <li class="list-group-item"><sup>ALC</sup>
+              {{beer.abv}}%
+              <sup>By Volume</sup></li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   `
 })
-export class AppComponent {}
+export class AppComponent {
+  beers: Beer[] = [
+    new Beer("Gear Up IPA", 14, 65, 6.2, "gearUp.png"),
+    new Beer("Organic hopworks", 14, 75, 6.6, "hub.png"),
+    new Beer("organic hub lager", 11.5, 32, 5.1, "hubLager.png"),
+    new Beer("NONSTOP HEF HOP", 10.5, 20, 3.9, "hefHop.png"),
+    new Beer("organic survival", 15, 35, 5.8, "stout.png"),
+    new Beer("Deluxe organic ale", 16, 55, 7.0, "DOA.png")
+  ];
+
+}
---
+++ app/beer.model.ts
@@ -0,0 +1,3 @@
+export class Beer {
+  constructor(public title: string, public plato: number, public ibu: number, public abv: number, public image: string) { }
+}
